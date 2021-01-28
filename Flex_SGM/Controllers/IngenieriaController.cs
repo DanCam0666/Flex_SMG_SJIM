1cce2650ec3421ca3182d655a929ab1caf0e8c0f
Luis Olivarez <LOlivarez@flexngate.com>
2021-01-28 12:39:51 -0600

    Modificacion para agregar apartado de ingenieria , se elimina datos de tablas inecesarias

---
+++ Flex_SGM/Controllers/IngenieriaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Flex_SGM.Controllers
+{
+    public class IngenieriaController : Controller
+    {
+        // GET: Ingenieria
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
