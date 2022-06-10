83314b6894cf47bcf1293e7ec05043b7bcbae0f6
DanCam0666 <dan.camacho@yahoo.com.mx>
2022-06-10 06:47:07 -0500

    Adding controllers and views

---
+++ Flex_SGM/Controllers/eoriginatorsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Flex_SGM.Models;
+
+namespace Flex_SGM.Controllers
+{
+    public class eoriginatorsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: eoriginators
+        public async Task<ActionResult> Index()
+        {
+            var eoriginators = db.eoriginators.Include(e => e.Areas);
+            return View(await eoriginators.ToListAsync());
+        }
+
+        // GET: eoriginators/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eoriginator eoriginator = await db.eoriginators.FindAsync(id);
+            if (eoriginator == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eoriginator);
+        }
+
+        // GET: eoriginators/Create
+        public ActionResult Create()
+        {
+            ViewBag.AreasID = new SelectList(db.cAreas, "ID", "Area");
+            return View();
+        }
+
+        // POST: eoriginators/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,AreasID,Supervisor")] eoriginator eoriginator)
+        {
+            if (ModelState.IsValid)
+            {
+                db.eoriginators.Add(eoriginator);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AreasID = new SelectList(db.cAreas, "ID", "Area", eoriginator.AreasID);
+            return View(eoriginator);
+        }
+
+        // GET: eoriginators/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eoriginator eoriginator = await db.eoriginators.FindAsync(id);
+            if (eoriginator == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AreasID = new SelectList(db.cAreas, "ID", "Area", eoriginator.AreasID);
+            return View(eoriginator);
+        }
+
+        // POST: eoriginators/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,AreasID,Supervisor")] eoriginator eoriginator)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eoriginator).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AreasID = new SelectList(db.cAreas, "ID", "Area", eoriginator.AreasID);
+            return View(eoriginator);
+        }
+
+        // GET: eoriginators/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eoriginator eoriginator = await db.eoriginators.FindAsync(id);
+            if (eoriginator == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eoriginator);
+        }
+
+        // POST: eoriginators/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            eoriginator eoriginator = await db.eoriginators.FindAsync(id);
+            db.eoriginators.Remove(eoriginator);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Flex_SGM/Controllers/ereasonsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Flex_SGM.Models;
+
+namespace Flex_SGM.Controllers
+{
+    public class ereasonsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ereasons
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.ereasons.ToListAsync());
+        }
+
+        // GET: ereasons/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ereason ereason = await db.ereasons.FindAsync(id);
+            if (ereason == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ereason);
+        }
+
+        // GET: ereasons/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ereasons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,Reason")] ereason ereason)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ereasons.Add(ereason);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(ereason);
+        }
+
+        // GET: ereasons/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ereason ereason = await db.ereasons.FindAsync(id);
+            if (ereason == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ereason);
+        }
+
+        // POST: ereasons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,Reason")] ereason ereason)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ereason).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(ereason);
+        }
+
+        // GET: ereasons/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ereason ereason = await db.ereasons.FindAsync(id);
+            if (ereason == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ereason);
+        }
+
+        // POST: ereasons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ereason ereason = await db.ereasons.FindAsync(id);
+            db.ereasons.Remove(ereason);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
